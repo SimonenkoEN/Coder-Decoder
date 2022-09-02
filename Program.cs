d7ef2ebe97a6e8c31c302204da164639e588314d
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-09-02 21:52:29 +0300

    Add 2 new method

--- Program.cs
+++ Program.cs
@@ -1,28 +1,43 @@
-﻿// Метод принимает на вход строку в коде Морзе м декодирует ее
-string Decoder(string inString) 
+﻿string[] arrMorse = { "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." };
+string[] arrSymbol = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+Console.WriteLine(MorseDecoder("-----   .---- ..---   ...-- ....- .....   -.... --... ---.. ----."));
+
+string MorseDecoder(string morseString)
 {
-    string[] arrMorse = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
-    string[] arrNumber = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    int index;
-    string outString = string.Empty;
-    string[] inArr = inString.Split("   ");
-    for (int i = 0; i < inArr.Length ; i++)
+    string decodeString = string.Empty;
+    string[] morseWordArray = morseString.Split("   ");
+    for (int i = 0; i < morseWordArray.Length; i++)
     {
-        string[] tmpArr = inArr[i].Split(" ");
-        for(int j = 0; j < tmpArr.Length; j++)
-        {
-            for (int k = 0; k < arrMorse.Length; k++)
-            {
-                if(tmpArr[j] == arrMorse[k]) 
-                {
-                    index = k;
-                    outString += arrNumber[index];
-                    break;
-                }                
-            }           
-        }
-        outString += " ";        
+        decodeString += GetWord(morseWordArray[i]) + " ";
     }
-    return outString;
+    return decodeString;
 }
-Console.WriteLine(Decoder(".- ----- .---- ..--- ...-- ....-   ---   ..... -.... --... ---.. ----. -.-"));
+
+string GetWord(string morseWord)
+{
+    string outWord = string.Empty;
+    string[] MorseSymbolArray = morseWord.Split(" ");
+    for (int i = 0; i < MorseSymbolArray.Length; i++)
+    {
+        outWord += GetSymbol(MorseSymbolArray[i]);
+    }
+    return outWord;
+}
+
+string GetSymbol(string outMorseSymbol)
+{
+    int index;
+    string outSymbol = string.Empty;
+
+    for (int i = 0; i < arrMorse.Length; i++)
+    {
+        if (arrMorse[i] == outMorseSymbol)
+        {
+            index = i;
+            outSymbol += arrSymbol[index];
+            break;
+        }
+    }
+    return outSymbol;
+}
